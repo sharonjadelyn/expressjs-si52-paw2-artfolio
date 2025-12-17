2f337387f8c3081d778eb65c6bca864b7ab3ae2e
GoDisCover <jessenco122@gmail.com>
2025-12-17 20:02:25 +0700

    Update Api.js, add collection

--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,17 @@ import * as userController from "../controllers/userController.js";
 import * as artworkController from "../controllers/artworkController.js";
 import { authenticateTokenMiddleware } from "../middlewares/authenticateTokenMiddleware.js";
 
-const router = express.Router();
+const api = express.Router();
 
 // AUTH
-router.post("/users/signup", userController.signUp);
-router.post("/users/signin", userController.signIn);
+api.post("/users/signup", userController.signUp);
+api.post("/users/signin", userController.signIn);
 
 // ARTWORK (PROTECTED)
-router.get("/artworks", authenticateTokenMiddleware, artworkController.artworks);
-router.post("/artworks", authenticateTokenMiddleware, artworkController.addNewArtwork);
-router.get("/artworks/:id", authenticateTokenMiddleware, artworkController.detailArtwork);
-router.put("/artworks/:id", authenticateTokenMiddleware, artworkController.updateArtwork);
-router.delete("/artworks/:id", authenticateTokenMiddleware, artworkController.deleteArtwork);
+api.get("/artworks", authenticateTokenMiddleware, artworkController.artworks);
+api.post("/artworks", authenticateTokenMiddleware, artworkController.addNewArtwork);
+api.get("/artworks/:id", authenticateTokenMiddleware, artworkController.detailArtwork);
+api.put("/artworks/:id", authenticateTokenMiddleware, artworkController.updateArtwork);
+api.delete("/artworks/:id", authenticateTokenMiddleware, artworkController.deleteArtwork);
 
-export default router;
+export default api;
